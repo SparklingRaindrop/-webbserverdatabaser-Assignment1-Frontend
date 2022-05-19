f05144de0671cfb9902e2fb0b75d9ddc46347e67
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-05-19 16:42:41 +0200

    Separate global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import { useEffect } from "react";
-
 import { ThemeProvider } from 'styled-components';
+
 import { themes } from './themeStyles';
-import { createGlobalStyle } from 'styled-components';
 import useApi from "./hooks/useApi";
 
-import img from './assets/background.svg';
-
+import { GlobalStyles } from './components/styles/GlobalStyles.styled';
 import { Title } from './components/styles/App.styled';
 import ToDoList from './components/ToDoList';
-import { useSetRecoilState } from "recoil";
-import { dataState } from "./recoil/data/atom";
-
-const GlobalStyles = createGlobalStyle`
-    body {
-        background: url(${img}) center center fixed;
-        background-size: cover;
-    }
-`
 
 export default function App() {
     const { isLoading, fetchData } = useApi();
