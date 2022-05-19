e28733211333369548a95bee808d3b027f0fca88
Tsubasa <tsubasa.fujii@cmeducations.se>
2022-05-19 20:11:01 +0200

    Add new configs to theme

--- src/themeStyles.js
+++ src/themeStyles.js
@@ -2,7 +2,28 @@ export const themes = {
     padding: '0.7rem 0.5rem',
     borderRadius: '0.2rem',
     colors: {
-        button: '#EB9BF5',
-        textBg: 'rgba(255, 255, 255, 0.8)',
+        background: {
+            normal: 'rgba(255, 255, 255, 0.7)',
+            hover: 'rgb(255, 255, 255)',
+            button: {
+                purple: 'rgb(235, 155, 245)',
+                hover: 'rgb(169, 220, 227, 0.2)',
+            }
+        }
+    },
+    typography: {
+        header: {
+
+        },
+        task: {
+            family: 'Inter, sans-serif',
+            weight: 600,
+            size: '1rem',
+        },
+        button: {
+            family: 'Inter, sans-serif',
+            weight: 400,
+            size: '0.8rem',
+        }
     }
 }
